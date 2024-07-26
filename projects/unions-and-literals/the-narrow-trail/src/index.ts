f977bcf1f53ed778cb1516335ab847366db7feba
stefafafan <github.le5ke@stenyan.jp>
2024-07-26 15:52:58 +0900

    implement runCommands

--- projects/unions-and-literals/the-narrow-trail/src/index.ts
+++ projects/unions-and-literals/the-narrow-trail/src/index.ts
@@ -1,3 +1,32 @@
 export function runCommands() {
-	// Declare your variables and runtime logic here! ✨
+	let availableResource: "Food" | "Water" | null = null;
+	let food = 5;
+	let water = 5;
+
+	for (let day = 1; day < 8; day++) {
+		let randomValue = Math.floor(6 * Math.random()) + 1;
+		if (randomValue === 1) {
+			availableResource = "Food";
+		}
+		if (randomValue === 2) {
+			availableResource = "Water";
+		}
+		if (randomValue >= 3) {
+			if (availableResource === "Food") {
+				food += randomValue;
+				availableResource = null;
+			} else if (availableResource === "Water") {
+				water += randomValue;
+				availableResource = null;
+			} else {
+				availableResource = randomValue % 2 == 0 ? "Food" : "Water";
+			}
+		}
+		food--;
+		water--;
+		if (food === 0 || water === 0) {
+			return false;
+		}
+	}
+	return true;
 }
